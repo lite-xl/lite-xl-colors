7cdfc6ea57a3c6f182bcf86ec1c02025d10d741a
Jan200101 <sentrycraft123@gmail.com>
2021-12-27 21:59:38 +0100

    rename monokai to monokai-pro, add mariana and monokai from Sublime

---
+++ colors/mariana.lua
@@ -0,0 +1,28 @@
+local style = require "core.style"
+local common = require "core.common"
+
+style.background = { common.color "#303841" }
+style.background2 = { common.color "#1d2227" }
+style.background3 = { common.color "#1d2227" }
+style.text = { common.color "#9ea191" }
+style.caret = { common.color "#61efce" }
+style.accent = { common.color "#ffd152" }
+style.dim = { common.color "#4c5863" }
+style.divider = { common.color "#242223" }
+style.selection = { common.color "#4c5863" }
+style.line_number = { common.color "#bfc5d0" }
+style.line_number2 = { common.color "#848b95" }
+style.line_highlight = { common.color "#303841" }
+style.scrollbar = { common.color "#696f75" }
+style.scrollbar2 = { common.color "#444b53" }
+
+style.syntax["normal"] = { common.color "#d7dde9" }
+style.syntax["symbol"] = { common.color "#d8dee9" }
+style.syntax["comment"] = { common.color "#a6acb9" }
+style.syntax["keyword"] = { common.color "#e55e66" }
+style.syntax["keyword2"] = { common.color "#ef6179" }
+style.syntax["number"] = { common.color "#ffd152" }
+style.syntax["literal"] = { common.color "#e75550" }
+style.syntax["string"] = { common.color "#939d5d" }
+style.syntax["operator"] = { common.color "#c2674f" }
+style.syntax["function"] = { common.color "#6699ca" }

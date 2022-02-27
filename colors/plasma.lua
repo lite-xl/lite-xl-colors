58a359cab61cd9ed292c8cbfa45a88012c7a1b5d
Chidambaram Nachiappan <60850111+thechidambaram@users.noreply.github.com>
2022-02-27 13:36:26 +0000

    Add 'plasma' color theme

---
+++ colors/plasma.lua
@@ -0,0 +1,29 @@
+local style = require "core.style"
+local common = require "core.common"
+
+style.background = { common.color "#222226" }
+style.background2 = { common.color "#252528" }
+style.background3 = { common.color "#1e1e21" }
+style.text = { common.color "#dddddd" }
+style.caret = { common.color "#aeafad" }
+style.accent = { common.color "#0097fb" }
+style.dim = { common.color "#9393a5" }
+style.divider = { common.color "#1E1E1E" }
+style.selection = { common.color "#264f78" }
+style.line_number = { common.color "#858585" }
+style.line_number2 = { common.color "#c6c6c6" }
+style.line_highlight = { common.color "#2b2b2f"}
+style.scrollbar = { common.color "#313136" }
+style.scrollbar2 = { common.color "#bfbfbf" }
+
+style.syntax["normal"] = { common.color "#dddddd" }
+style.syntax["symbol"] = { common.color "#e06c75" }
+style.syntax["comment"] = { common.color "#c5c5c5" }
+style.syntax["keyword"] = { common.color "#61afef" }
+style.syntax["keyword2"] = { common.color "#56B6C2" }
+style.syntax["number"] = { common.color "#d19a66" }
+style.syntax["literal"] = { common.color "#61AFEF" }
+style.syntax["string"] = { common.color "#98C379" }
+style.syntax["operator"] = { common.color "#dddddd" }
+style.syntax["function"] = { common.color "#c678dd" }
+

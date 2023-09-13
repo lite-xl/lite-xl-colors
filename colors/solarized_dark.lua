547e00c776115ac9a89f179fc501ff295f2fcf9d
VannRR <92082133+VannRR@users.noreply.github.com>
2023-09-12 22:24:40 -0700

    Add solarized_dark theme

---
+++ colors/solarized_dark.lua
@@ -0,0 +1,28 @@
+local style = require "core.style"
+local common = require "core.common"
+
+style.background = { common.color "#002b36" }
+style.background2 = { common.color "#073642" }
+style.background3 = { common.color "#073642" }
+style.text = { common.color "#93A1A1" }
+style.caret = { common.color "#586e75" }
+style.accent = { common.color "#268bd2" }
+style.dim = { common.color "#586e75" }
+style.divider = { common.color "#23424A" }
+style.selection = { common.color "#073642" }
+style.line_number = { common.color "#586e75" }
+style.line_number2 = { common.color "#93A1A1" }
+style.line_highlight = { common.color "#073642" }
+style.scrollbar = { common.color "#304A50" }
+style.scrollbar2 = { common.color "#465356" }
+
+style.syntax["normal"] = { common.color "#93A1A1" }
+style.syntax["symbol"] = { common.color "#93A1A1" }
+style.syntax["comment"] = { common.color "#586e75" }
+style.syntax["keyword"] = { common.color "#859900" }
+style.syntax["keyword2"] = { common.color "#268bd2" }
+style.syntax["number"] = { common.color "#d33682" }
+style.syntax["literal"] = { common.color "#2aa198" }
+style.syntax["string"] = { common.color "#2aa198" }
+style.syntax["operator"] = { common.color "#859900" }
+style.syntax["function"] = { common.color "#268bd2" }

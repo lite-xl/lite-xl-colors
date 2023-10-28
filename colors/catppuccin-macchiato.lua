6fbba0a8c1bb85bf11da286ddfdfa07bb472eac6
SagXD <95903022+SagXD@users.noreply.github.com>
2023-10-28 14:17:32 +0530

    Added Themes

---
+++ colors/catppuccin-macchiato.lua
@@ -0,0 +1,29 @@
+local style = require "core.style"
+local common = require "core.common"
+
+style.background = { common.color "#24273a" }
+style.background2 = { common.color "#1e2030" }
+style.background3 = { common.color "#363a4f" }
+style.text = { common.color "#cad3f5" }
+style.caret = { common.color "#f4dbd6" }
+style.accent = { common.color "#c6a0f6" }
+style.dim = { common.color "#b8c0e0" }
+style.divider = { common.color "#c6a0f6" }
+style.selection = { common.color "#5b6078" }
+style.line_number = { common.color "#6e738d" }
+style.line_number2 = { common.color "#c6a0f6" }
+style.line_highlight = { common.color "#363a4f" }
+style.scrollbar = { common.color "#5b6078" }
+style.scrollbar2 = { common.color "#6e738d" }
+style.scrollbar_track = { common.color "#1e2030" }
+
+style.syntax["normal"] = { common.color "#ed8796" }
+style.syntax["symbol"] = { common.color "#b7bdf8" }
+style.syntax["comment"] = { common.color "#6e738d" }
+style.syntax["keyword"] = { common.color "#ed8796" }
+style.syntax["keyword2"] = { common.color "#076678" }
+style.syntax["number"] = { common.color "#f5a97f" }
+style.syntax["literal"] = { common.color "#b7bdf8" }
+style.syntax["string"] = { common.color "#a6da95" }
+style.syntax["operator"] = { common.color "#91d7e3" }
+style.syntax["function"] = { common.color "#8aadf4" }

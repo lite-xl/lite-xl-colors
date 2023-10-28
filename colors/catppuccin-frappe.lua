8cb27b437536ea85065abdc2269605ffd2b0047c
SagXD <95903022+SagXD@users.noreply.github.com>
2023-10-28 16:16:43 +0530

    Added Catppuccin Themes (#30)
    * Added Themes
    
    * Added preview
    
    * Delete previews/catppuccin-mocha_preview.png
    
    * Delete previews/catppuccin-macchiato_preview.png
    
    * Delete previews/catppuccin-frappe_preview.png
    
    * Delete previews/catppuccin-latte_preview.png
    
    * Added Preview
    
    * Update manifest.json
    
    * Update manifest.json
    
    * [CI] Updated README.md.
    
    * Delete previews/catppuccin-frappe.png
    
    * Delete previews/catppuccin-latte.png
    
    * Delete previews/catppuccin-macchiato.png
    
    * Delete previews/catppuccin-mocha.png
    
    * Update manifest.json
    
    * Update manifest.json
    
    * [CI] Updated README.md.
    
    ---------
    
    Co-authored-by: Github Actions <>

---
+++ colors/catppuccin-frappe.lua
@@ -0,0 +1,29 @@
+local style = require "core.style"
+local common = require "core.common"
+
+style.background = { common.color "#303446" }
+style.background2 = { common.color "#292c3c" }
+style.background3 = { common.color "#414559" }
+style.text = { common.color "#c6d0f5" }
+style.caret = { common.color "#f2d5cf" }
+style.accent = { common.color "#ca9ee6" }
+style.dim = { common.color "#b5bfe2" }
+style.divider = { common.color "#ca9ee6" }
+style.selection = { common.color "#626880" }
+style.line_number = { common.color "#737994" }
+style.line_number2 = { common.color "#ca9ee6" }
+style.line_highlight = { common.color "#414559" }
+style.scrollbar = { common.color "#626880" }
+style.scrollbar2 = { common.color "#737994" }
+style.scrollbar_track = { common.color "#292c3c" }
+
+style.syntax["normal"] = { common.color "#e78284" }
+style.syntax["symbol"] = { common.color "#babbf1" }
+style.syntax["comment"] = { common.color "#737994" }
+style.syntax["keyword"] = { common.color "#e78284" }
+style.syntax["keyword2"] = { common.color "#e78284" }
+style.syntax["number"] = { common.color "#ef9f76" }
+style.syntax["literal"] = { common.color "#babbf1" }
+style.syntax["string"] = { common.color "#a6d189" }
+style.syntax["operator"] = { common.color "#99d1db" }
+style.syntax["function"] = { common.color "#8caaee" }
---
+++ colors/catppuccin-latte.lua
@@ -0,0 +1,29 @@
+local style = require "core.style"
+local common = require "core.common"
+
+style.background = { common.color "#eff1f5" }
+style.background2 = { common.color "#e6e9ef" }
+style.background3 = { common.color "#ccd0da" }
+style.text = { common.color "#4c4f69" }
+style.caret = { common.color "#dc8a78" }
+style.accent = { common.color "#8839ef" }
+style.dim = { common.color "#5c5f77" }
+style.divider = { common.color "#8839ef" }
+style.selection = { common.color "#bcc0cc" } -- surface1
+style.line_number = { common.color "#9ca0b0" }
+style.line_number2 = { common.color "#8839ef" }
+style.line_highlight = { common.color "#dce0e8" } -- crust
+style.scrollbar = { common.color "#acb0be" }
+style.scrollbar2 = { common.color "#9ca0b0" }
+style.scrollbar_track = { common.color "#dce0e8" } -- crust
+
+style.syntax["normal"] = { common.color "#d20f39" }
+style.syntax["symbol"] = { common.color "#7287fd" }
+style.syntax["comment"] = { common.color "#9ca0b0" }
+style.syntax["keyword"] = { common.color "#d20f39" }
+style.syntax["keyword2"] = { common.color "#d20f39" }
+style.syntax["number"] = { common.color "#fe640b" }
+style.syntax["literal"] = { common.color "#7287fd" }
+style.syntax["string"] = { common.color "#40a02b" }
+style.syntax["operator"] = { common.color "#04a5e5" }
+style.syntax["function"] = { common.color "#1e66f5" }

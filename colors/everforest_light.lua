9e954b0eec8d193305c0e3b8e0f1440959dfd95b
kramo <93832451+kra-mo@users.noreply.github.com>
2024-05-14 21:39:06 +0200

    Everforest improvements + Everforest light (#41)
    * everforest: Fix scrollbar color
    
    Make scrollbar light on dark instead of dark on light
    
    * everforest_light: Add theme

---
+++ colors/everforest_light.lua
@@ -0,0 +1,31 @@
+local style = require "core.style"
+local common = require "core.common"
+
+style.background = { common.color "#FFFBEF"}
+style.background2 = { common.color "#FFFBEF"}
+style.background3 = { common.color "#FFFBEF"}
+style.text = { common.color "#5C6A72"}
+style.caret = { common.color "#93B259"}
+style.accent = { common.color "#93B259"}
+style.dim = { common.color "#939F91"}
+style.divider = { common.color "#E8E5D5"}
+style.selection = { common.color "#E8E5D5"}
+style.line_number = {common.color "#BEC5B2"}
+style.line_number2 = {common.color "#829181"}
+style.line_highlight = {common.color "#F8F5E4"}
+style.scrollbar = { common.color "#BEC5B2"}
+style.scrollbar2 = { common.color "#BEC5B2"}
+style.scrollbar_track = { common.color "#EDEADA"}
+style.drag_overlay = { common.color "#BEC5B233"}
+
+style.syntax["normal"] = { common.color "#5C6A72"}
+style.syntax["symbol"] = { common.color "#5C6A72"}
+style.syntax["comment"] = {common.color "#A6B0A0"}
+style.syntax["keyword"] = {common.color "#F85552"}
+style.syntax["keyword2"] = {common.color "#F85552"}
+style.syntax["number"] = {common.color "#DF69BA"}
+style.syntax["literal"] = {common.color "#3A94C5"}
+style.syntax["string"] = {common.color "#35A77C"}
+style.syntax["operator"] = {common.color "#DFA000"}
+style.syntax["function"] = {common.color "#8DA101"}
+
